50c99a4440f548ce57e6f6e68bb3805a659841a5
Yusuf <yusuf_zeki1234@hotmail.com>
2023-06-07 11:49:24 +0300

    Bottom Weapons Updated

---
+++ Assets/SecondLevel/Scripts/TowerScripts/ArcherTowerBottom.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+
+public class ArcherTowerBottom : MonoBehaviour
+{
+    [Header("Upgrade Tower")]
+    [SerializeField] GameObject upgradeTower;
+    [Space]
+    private Transform effectPos;
+    public ArrowBottom Bullet;
+    public float nextPrefab;
+    public GameObject bombEffect;
+    BoatController boat;
+    private AudioSource audio;
+    public AudioClip bomb;
+    public LayerMask enemyLayer; // Düþmanlarýn bulunduðu katman
+    bool fire = false;
+    private void Start()
+    {
+        boat = GameObject.FindGameObjectWithTag("Ship").gameObject.GetComponent<BoatController>();
+        audio = GetComponent<AudioSource>();
+        effectPos = GetComponentInChildren<Transform>();
+    }
+
+
+    private void Update()
+    {
+        nextPrefab += Time.deltaTime;
+        int facing;
+        if (boat.transform.localScale.x == 1)
+        {
+            facing = -1;
+            transform.localScale = new Vector2(-1, 1);
+        }
+        else
+        {
+            facing = 1;
+            transform.localScale = new Vector2(-1, 1);
+        }
+
+        RaycastHit2D[] hitEnemies = Physics2D.LinecastAll(transform.position, new Vector2(transform.position.x + (10 * facing), transform.position.y), enemyLayer);
+
+        foreach (RaycastHit2D hit in hitEnemies)
+        {
+            Collider2D enemy = hit.collider;
+            
+            if (enemy != null)
+            {
+                fire = true;
+                if (nextPrefab >= 1.5f && fire)
+                {
+                    ProjectTile(enemy.transform);
+                    nextPrefab = 0;
+                    audio.PlayOneShot(bomb);
+                    Instantiate(bombEffect, effectPos.position, Quaternion.identity);
+                }
+            }
+            else
+            {
+                fire = false;
+            }
+        }
+
+    }
+
+    public void ProjectTile(Transform enemy)
+    {
+        GameObject row = Instantiate(Bullet,transform.position,transform.rotation).gameObject;
+        if (boat.transform.localScale.x == 1)
+        {
+            row.GetComponent<Transform>().localScale = new Vector2(-1,1);
+        }
+        else
+        {
+            row.GetComponent<Transform>().localScale = new Vector2(1,1);
+        }
+        ArrowBottom arrow = row.GetComponent<ArrowBottom>();
+        arrow.SetTarget(enemy);
+    }
+
+    private void OnDrawGizmos()
+    {
+        int facing;
+        if (boat.transform.localScale.x == 1)
+        {
+            facing = -1;
+            transform.localScale = new Vector2(-1, 1);
+        }
+        else
+        {
+            facing = 1;
+            transform.localScale = new Vector2(1, 1);
+        }
+
+        Gizmos.color = UnityEngine.Color.red;
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x + (10 * facing), transform.position.y));
+    }
+
+    public void UpgradeTowerButton()
+    {
+        Destroy(gameObject);
+        Instantiate(upgradeTower, transform.position, Quaternion.identity);
+        GetComponentInChildren<Try>().CloseTower();
+    }
+}
---
+++ Assets/SecondLevel/Scripts/TowerScripts/ArrowBottom.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowBottom : MonoBehaviour
+{
+    [Header("References")]
+    public Rigidbody2D rb;
+
+    [Header("Attributes")]
+    public float rowSpeed = 5f;
+    public int Damage;
+    public string Name;
+
+    public Transform target;
+    private void Start()
+    {
+        Damage = 15;
+    }
+
+    public void SetTarget(Transform _target)
+    {
+        target = _target;
+    }
+    private void FixedUpdate()
+    {
+        if (target)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, rowSpeed * Time.deltaTime);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("Clamb"))
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("Clamb"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
