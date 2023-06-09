13822d79e2862bbd145845ff23a55cd4ee4c0115
Recep Can Saraç <113214739+RecepCanSarac@users.noreply.github.com>
2023-06-09 20:24:31 +0300

    Update SecendLevelSpawnPointScripts.cs

--- Assets/Fourdlevel/Scriptss/SecendLevelSpawnPointScripts.cs
+++ Assets/Fourdlevel/Scriptss/SecendLevelSpawnPointScripts.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class SecendLevelSpawnPointScripts : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject tankEnemy;
+    public GameObject SmallEnemy;
+    public GameObject DrunkEnemy;
+    public GameObject GirdapEnemy;
+    public GameObject FlyEnemy;
+    public GameObject clambEnemy;
+
+
+    private Vector2 SeaPoint;
+
+
+
+
+
+
+    private Transform target;
+
+    private void Start()
     {
-        
+        SpawnerTimer();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SpawnerTwo()
     {
-        
+        StartCoroutine("SpawnerTimer");
     }
+
+
+    IEnumerator SpawnerTimer()
+    {
+        //Wave 1
+        for (int i = 0; i < 5; i++)
+        {
+            int random1 = Random.Range(0, 2);
+            if (random1 == 0)
+            {
+                target = GameObject.FindGameObjectWithTag("Ship").transform;
+                SeaPoint = new Vector2(target.position.x + 10f,-4f);
+                Instantiate(tankEnemy,SeaPoint,Quaternion.identity);
+            }
+            else if (random1 == 1)
+            {
+                target = GameObject.FindGameObjectWithTag("Ship").transform;
+                SeaPoint = new Vector2(target.position.x - 10f, -4f);
+                Instantiate(tankEnemy, SeaPoint, Quaternion.identity);
+            }
+            for (int a = 0; a < 3; a++)
+            {
+                Instantiate(SmallEnemy, SeaPoint, Quaternion.identity);
+                yield return new WaitForSeconds(2);
+            }
+        }
+        yield return new WaitForSeconds(12);
+        //Wave 2
+    }
+
 }
